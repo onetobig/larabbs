210e3b20567c599a1c4ef713d16f571009f4dfd3
onetobig <a123a123>
2018-01-25 16:18:38 +0800

    Finish basic page layout

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,3 +5,8 @@
  * Date: 2018/1/23
  * Time: 14:34
  */
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title', '首页')
+
+@section('content')
+@stop
