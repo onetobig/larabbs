8eb2e904f8bb57ef29c122e02f9b6d691385a405
onetobig <a123a123>
2018-02-01 11:04:47 +0800

    List user replies

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -23,7 +23,7 @@
 					{{--话题列表--}}
 					@include('topics._topic_list', ['topics', $topics])
 					{{-- 分页 --}}
-					{!! $topics->render() !!}
+					{!! $topics->appends(Request::except('page'))->render() !!}
 				</div>
 			</div>
 		</div>
