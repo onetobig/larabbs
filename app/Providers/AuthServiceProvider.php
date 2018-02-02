8043d101e3dcf92080a202700b109226bf6fc016
onetobig <a123a123>
2018-02-02 16:20:00 +0800

    Add manage contents permission and visit horizon dashboard permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,6 +28,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        \Horizon::auth(function ($request){
+            // 判断是否是站长
+            return \Auth::user()->hasRole('Founder');
+        });
     }
 }
