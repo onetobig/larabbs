efd90043be67adebe4bbe764b5c9b3035c870f63
onetobig <a123a123>
2018-02-09 15:15:20 +0800

    Sidebar links

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,20 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $request, Topic $topic, User $user)
+    public function show(Category $category, Request $request, Topic $topic, User $user, Link $link)
     {
         $topics = $topic->where('category_id', $category->id)
             ->withOrder($request->order)
             ->paginate(20);
 
         $active_users = $user->getActiveUsers();
+
+        // 资源链接
+        $links = $link->getAllCached();
+
         // 活跃用户列表
-        return view('topics.index', compact('topics', 'category', 'active_users'));
+        return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }
