0b820419ff4dce3ad6ddfb6bd691d04296815a7c
onetobig <onetobig@gmail.com>
2018-05-29 11:24:40 +0800

    活跃用户列表

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -58,4 +58,9 @@ class UsersController extends Controller
         $user->update($attributes);
         return $this->response->item($user, new UserTransformer());
     }
+
+    public function activedIndex(User $user)
+    {
+        return $this->response->collection($user->getActiveUsers(), new UserTransformer());
+    }
 }
