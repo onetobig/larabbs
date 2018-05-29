0723062b181cb1b8ef6c37e2cc7dc44c14985b8b
onetobig <onetobig@gmail.com>
2018-05-29 11:40:57 +0800

    本地化，多语言支持

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -12,8 +12,14 @@ use App\Http\Requests\Request;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Controller extends BaseController
 {
     use Helpers,AuthorizesRequests;
+
+    public function errorResponse($statusCode, $message = null, $code = 0)
+    {
+        throw new HttpException($statusCode, $message, null, [], $code);
+    }
 }
