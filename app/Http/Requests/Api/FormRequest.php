58c217ce4bd30b468b3d00720ccfda0b2c7afa97
onetobig <onetobig@gmail.com>
2018-05-31 10:47:43 +0800

    api request 修正验证错误报 500

--- app/Http/Requests/Api/FormRequest.php
+++ app/Http/Requests/Api/FormRequest.php
@@ -2,15 +2,11 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
+use Dingo\Api\Http\FormRequest as BaseFormRequest;
+#use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
 
 class FormRequest extends BaseFormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
