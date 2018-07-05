f111518ac4e9a953c7a3ca71d53b531a13e5dbec
onetobig <onetobig@gmail.com>
2018-07-05 15:27:58 +0800

    topics store

---
+++ app/Transformers/TopicsTransformer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/5
+ * Time: 15:17
+ */
+
+namespace App\Transformers;
+
+
+use App\Models\Topic;
+use League\Fractal\TransformerAbstract;
+
+class TopicsTransformer extends TransformerAbstract
+{
+    public function transform(Topic $item)
+    {
+        return [
+            'id' => $item->id,
+            'title' => $item->title,
+            'body' => $item->body,
+            'user_id' => (int)$item->user_id,
+            'category_id' => (int)$item->category_id,
+            'reply_count' => (int)$item->reply_count,
+            'view_count' => (int)$item->view_count,
+            'last_reply_user_id' => (int)$item->last_reply_user_id,
+            'except' => $item->except,
+            'slug' => $item->slug,
+            'created_at' => $item->created_at->toDateTimeString(),
+            'updated_at' => $item->updated_at->toDateTimeString(),
+        ];
+    }
+}
