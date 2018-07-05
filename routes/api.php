9759e2730115e5e7d02e8ac51b9c7bc0136e4ccc
onetobig <onetobig@gmail.com>
2018-07-05 15:46:24 +0800

    topics update api

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ $api = app(\Dingo\Api\Routing\Router::class);
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' =>  ['serializer:array'],
+    'middleware' =>  ['serializer:array', 'bindings'],
 ], function ($api) {
 
     $api->group([
@@ -69,6 +69,8 @@ $api->version('v1', [
             // 发布话题
             $api->post('topics', 'TopicsController@store')
                 ->name('api.topics.store');
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
         });
     });
 
